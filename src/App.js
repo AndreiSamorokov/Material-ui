27621d283232862e85b8cb94932c353310f1bc0f
AndreiSamorokov <andreiv0202@gmail.com>
2020-11-04 02:40:30 +0900

    add icons

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+import GoIcon from '@material-ui/icons/Flag'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="contained" color="secondary">
+        <Button 
+          startIcon = {<SaveIcon />}
+          endIcon = {<GoIcon />}
+          size="large" 
+          style={{fontSize : 20}}
+          variant="contained" 
+          color="secondary"
+        >
           Secondary
         </Button>
       </header>
